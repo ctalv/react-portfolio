6a4e89ab2b523d9f5cb37e3cf390779705f9bd1c
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-14 16:12:05 -0500

    added App.js content

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+export default function App() {
+    return (
+        <div>
+            <Header/>
+            <Navigation/>
+            <Project/>
+            <Footer/>
+        </div>
+    )
+}
