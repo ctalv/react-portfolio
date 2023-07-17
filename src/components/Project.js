2d558f86a8712c69981238b7ad7cdd9c5b11261f
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-17 15:23:45 -0500

    added base for project componenet and portfolio page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,11 @@
 // A single Project component that will be used multiple times in the Portfolio section
 
 // child of Portfolio page; logic for links and event handling for individual projects
+const projects = [
+    {
+        title: '',
+        link: '',
+        image: '',
+        alt: ''
+    }
+]
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,21 +1,26 @@
 // activites 13 and 14 for event handling
+// activities 9 and 10
 
 // parent of project component
 
 import React from 'react';
 
-export default function About() {
+export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <section>
+        {projects.map((project) => (
+          <div>
+            <h3>{`${project.title}`}</h3>
+            <a href={project.link}>
+              <img src={project.image} alt={project.alt} />
+            </a>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
+
+
