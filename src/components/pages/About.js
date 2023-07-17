7b8ea603770274dd32185286ee7b625f0d97fed5
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-17 14:57:29 -0500

    added about me page; still need image

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,13 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <img src="./assets/images/headshot.JPG" />
       <p>
+        My passion has always been science. From a young age, I spent all my time outside
+        exploring the natural world. As I grew older, I began developing an interest in 
+        video games and learning about computers. It was not until I took a college course
+        in the MATLAB coding language that I realized coding is my thing. 
+
         I am a recent graduate from the University of Houston with a BS in Biology. 
         In August of 2023, I completed a Full Stack Web Development course from Rice
         University's Glassock School of Continuing Education.
