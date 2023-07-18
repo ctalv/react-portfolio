ffa16db2b69caa2495f804d0d9eafeacdca92a3f
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-18 18:19:18 -0500

    added portfolio styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,17 +6,45 @@
 import React from 'react';
 import projects from '../Project'
 
+const styles = {
+  portfolioStyle: {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridTemplateRows: 'min-content'
+  },
+  projectStyle: {
+    position: 'relative',
+    // backgroundColor: '#f6f7e5',
+    padding: '3px',
+    margin: '7px',
+  },
+  projectImageStyle: {
+    display: 'block',
+    objectFit: 'cover',
+    width: '100%',
+    height: '300px',
+  },
+  projectHeadingStyle: {
+    position: 'absolute',
+    background: 'rgba(0, 0, 0, 0.5)',
+    color: '#f6f7e5',
+    width: '75%',
+    fontSize: '20px',
+    textAlign: 'center',
+  }
+};
+
 export default function Portfolio() {
 
   return (
     <div>
       <h2>Portfolio</h2>
-      <section>
+      <section style={styles.portfolioStyle}>
         {projects.map((project) => (
-          <div>
-            <h3>{`${project.title}`}</h3>
+          <div style={styles.projectStyle}>
+            <h3 style={styles.projectHeadingStyle} >{`${project.title}`}</h3>
             <a href={project.link}>
-              <img src={project.image} alt={project.alt} />
+              <img src={project.image} alt={project.alt} style={styles.projectImageStyle} />
             </a>
           </div>
         ))}
