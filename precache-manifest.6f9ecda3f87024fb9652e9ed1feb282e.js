837e32593b0dd84d6f95c580215e9a13b29bea14
ctalv <122413805+ctalv@users.noreply.github.com>
2023-07-20 21:06:10 -0500

    Updates

---
+++ precache-manifest.6f9ecda3f87024fb9652e9ed1feb282e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaf088c17e48187a0099",
+    "url": "/react-portfolio/static/css/main.2bf3c37a.chunk.css"
+  },
+  {
+    "revision": "aaf088c17e48187a0099",
+    "url": "/react-portfolio/static/js/main.ef18f44c.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "76c974a4afbb08118afa",
+    "url": "/react-portfolio/static/js/2.267763ac.chunk.js"
+  },
+  {
+    "revision": "4dc015b3b480aa99418bd44a9dbee98a",
+    "url": "/react-portfolio/static/media/headshot.4dc015b3.JPG"
+  },
+  {
+    "revision": "1e773c76a7d6a2110f2e8b893de14a9b",
+    "url": "/react-portfolio/static/media/code-markus-spiske-iar-afB0QQw-unsplash.1e773c76.jpg"
+  },
+  {
+    "revision": "f8e0b18a84bed7b723e2b72a98c2586d",
+    "url": "/react-portfolio/static/media/desktop-joshua-aragon-BMnhuwFYr7w-unsplash.f8e0b18a.jpg"
+  },
+  {
+    "revision": "3fb3a162851d7d13d7399141f7299b38",
+    "url": "/react-portfolio/static/media/project_1.3fb3a162.JPG"
+  },
+  {
+    "revision": "8f72e62d066bceab47a68b8009dfeffc",
+    "url": "/react-portfolio/static/media/project_2.8f72e62d.JPG"
+  },
+  {
+    "revision": "3b75d1b0443d1e3a8686563268cd1946",
+    "url": "/react-portfolio/static/media/resume.3b75d1b0.pdf"
+  },
+  {
+    "revision": "f4c62495beafb8d2b68fbdec0cee1ded",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-// A single Footer component that appears on multiple pages
-
-import React from 'react';
-
-export default function Header() {
-    return (
-            <h3>Powered by React</h3>
-    );
-}
