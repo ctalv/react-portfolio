f056539189831f1a56d7ea47a73e762cf726ae71
ctalv <122413805+ctalv@users.noreply.github.com>
2023-08-11 11:36:35 -0500

    added project 3

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,22 +3,29 @@ import portfolioPic from '../assets/images/code-markus-spiske-iar-afB0QQw-unspla
 import githubPic from '../assets/images/desktop-joshua-aragon-BMnhuwFYr7w-unsplash.jpg'
 import projectOnePic from '../assets/images/project_1.JPG'
 import projectTwoPic from '../assets/images/project_2.JPG'
+import projectThreePic from '../assets/images/project_3.jpg'
 
 
 
 // child of Portfolio page; logic for links and event handling for individual projects
 const projects = [
     {
-        title: 'Movie Match Maker',
-        link: 'https://adamhood15.github.io/movie-match-maker/',
-        image: projectOnePic,
-        alt: 'Screenshot of Movie Match Maker homepage'
+        title: 'Zaza Lingerie Ecommerce',
+        link: 'https://quiet-caverns-99335-f8f9f8c7d100.herokuapp.com/',
+        image: projectThreePic,
+        alt: 'Homescreen of Zaza'
     },
     {
         title: 'The Yum Yum Blog',
         link: 'https://yumyum-blog.herokuapp.com/',
         image: projectTwoPic,
         alt: 'Screenshot of The Yum Yum Blog homepage'
+    },
+    {
+        title: 'Movie Match Maker',
+        link: 'https://adamhood15.github.io/movie-match-maker/',
+        image: projectOnePic,
+        alt: 'Screenshot of Movie Match Maker homepage'
     },
     {
         title: 'GitHub',
